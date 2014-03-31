ea1a46000af83eb601910bab3b8eb7580075259e
Tibor Molnár <fatalaa@hotmail.com>
2014-03-31 11:41:47 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='xml-miner',
+    version='0.1.0',
+    packages=['xmlminer', 'xmlminer.tests', 'xmlminer.mining', 'xmlminer.arial10', 'xmlminer.providers'],
+    url='https://github.com/fatalaa/xml-miner.git',
+    license='MIT',
+    author='Tibor',
+    author_email='motibi89@yahoo.com',
+    description=''
+)
