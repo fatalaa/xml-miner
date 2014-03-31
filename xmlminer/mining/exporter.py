943650b57e10e6fce84b1056143ea936b514520f
Tibor Molnár <fatalaa@hotmail.com>
2014-03-31 14:13:31 +0200

    added async processing function

--- xmlminer/mining/exporter.py
+++ xmlminer/mining/exporter.py
@@ -2,7 +2,13 @@ __author__ = 'Tibor'
 import xlwt
 from xmlminer.providers.provider import ShoppingDotComProvider, WalMartProvider
 from fitsheetwrapper import FitSheetWrapper
+from threading import Thread
 
+def async(function):
+    def wrapper(*args, **kwargs):
+        thread = Thread(target=function, args=args, kwargs=kwargs)
+        thread.start()
+    return wrapper
 
 class Exporter():
     def __init__(self):
@@ -15,10 +21,15 @@ class Exporter():
         li.append(('Walmart.com', WalMartProvider()))
         return li
 
-    def export(self, filename='categories.xls'):
+    def export(self, filename):
         for provider in self.providers:
             root = provider[1].get_category_xml()
             sheet = FitSheetWrapper(self.workbook.add_sheet(provider[1].name))
             provider[1].row = 0
             provider[1].process(0, root, sheet, 0, provider[1].providerInfoIndex)
             self.workbook.save(filename)
+
+
+    @async
+    def export_async(self, filename='categories.xls'):
+        self.export(filename)
